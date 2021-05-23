40c2dbe94972c53c08bec03bc793a53e1a221eed
yair-mantis <yair@mantis-vision.com>
2021-05-24 01:36:57 +0300

    py3 converion

--- README.md
+++ README.md
@@ -10,3 +10,22 @@ using ffmpeg to read the mjpeg stream from the [9$ cam](https://wiki.idiot.io/es
 pipe it to opencv and route to ...
 
 
+### gstreamer
+gst-launch-1.0 souphttpsrc \
+    location=http://192.168.0.85:81 do-timestamp=true \
+    ! multipartdemux ! image/jpeg,width=640,height=480 \
+    ! matroskamux \
+    ! filesink location=mjpeg.mkvD
+
+ gst-launch-1.0 videotestsrc is-live=true ! openh264enc ! mpegtsmux \
+  ! hlssink playlist-root=https://your-site.org \
+  location=/srv/hls/hlssink.%05d.ts \
+  playlist-location=/srv/hls/playlist.m3u8
+
+gst-launch-1.0 souphttpsrc \
+    location=http://192.168.0.85:81 do-timestamp=true \
+    ! multipartdemux ! image/jpeg,width=640,height=480 \
+    ! openh264enc ! mpegtsmux \
+    ! hlssink playlist-root=https://your-site.org \
+    location=/srv/hls/hlssink.%05d.ts \
+    playlist-location=/srv/hls/playlist.m3u8
--- ffmpeg_pipes_cv2.py
+++ ffmpeg_pipes_cv2.py
@@ -5,18 +5,11 @@ import tempfile
 import subprocess
 import cv2
 import numpy as np
-
-import Image
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-from SocketServer import ThreadingMixIn
-import StringIO
-import time
 import datetime
-
 
 # To get this path execute:
 #    $ which ffmpeg
-FFMPEG_BIN = '/home/user/anaconda3/envs/opencv/bin/ffmpeg'
+FFMPEG_BIN = '/home/user/anaconda3/envs/cv2.39/bin/ffmpeg'
 
 
 # To find allowed formats for the specific camera:
@@ -39,6 +32,7 @@ def run_ffmpeg():
     return subprocess.Popen(ffmpg_cmd, stdout = subprocess.PIPE, bufsize=10**8)
 
 def run_cv_window(process):
+    avg = None
     while True:
         # read frame-by-frame
         raw_image = process.stdout.read(600*800*3)
@@ -46,11 +40,58 @@ def run_cv_window(process):
             raise RuntimeError("Empty pipe")
         
         # transform the bytes read into a numpy array
-        frame =  np.frombuffer(raw_image, dtype='uint8')
-        frame = frame.reshape((600,800,3)) # height, width, channels
-        if frame is not None:
-            cv2.imshow('Video', frame)
+        frameBuf =  np.frombuffer(raw_image, dtype='uint8')
+        frameBuf = frameBuf.reshape((600,800,3)) # height, width, channels
+        if frameBuf is not None:
+            
         
+            gray = cv2.cvtColor(frameBuf, cv2.COLOR_BGR2GRAY)
+            #####
+            frame = cv2.GaussianBlur(gray, (21, 21), 0)
+            #text = "Unoccupied"
+            if avg is None:
+                avg = frame.copy().astype("float")
+                continue
+            cv2.accumulateWeighted(frame, avg, 0.5)
+            myDelta = cv2.absdiff(frame, cv2.convertScaleAbs(avg))
+            thresh = cv2.threshold(myDelta, 5, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+            result = []
+            for c in cnts:
+                if cv2.contourArea(c) >= 900:
+                    result.append(c)
+                    #text = "Occupied"
+
+            if result or not ('imgRGB' in locals()):
+                #####
+                edge = cv2.blur(gray, (3, 3))
+                edgeThresh = 40
+                edge = cv2.Canny(edge,
+                                edgeThresh,
+                                edgeThresh * 3,
+                                apertureSize=3)
+                edge = cv2.dilate(edge, np.ones((3, 3), np.uint8))
+                imgRGB = cv2.bitwise_and(frameBuf, frameBuf, mask=edge)
+                imgRGB = cv2.cvtColor(imgRGB, cv2.COLOR_BGR2RGB)
+                #####
+                for c in result:
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    imgRGB[y:y + h,
+                        x:x + w] = np.random.randint(256,
+                                                        size=(h, w, 3))
+                #### DOWN WITH BIG BROTHER ###
+                #cv2.rectangle(imgRGB, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                #cv2.rectangle(imgRGB, (x, y), (x + w, y + h), (0, 255, 0), -1)
+                #cv2.putText(imgRGB, "{}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                # ts = datetime.datetime.now(datetime.timezone(
+                    # 'Asia/Jerusalem')).strftime("%A %d %B %Y %I:%M:%S%p")
+                ts = "hi"
+                cv2.putText(imgRGB, ts, (10, imgRGB.shape[0] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 0, 0), 1)
+                cv2.imshow('Video', imgRGB)
+                                
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.imwrite("tomatoOut.bmp", frame)
             break
